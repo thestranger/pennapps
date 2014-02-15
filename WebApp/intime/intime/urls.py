0995746f80166b1dd712dd8d470830e799779d9f
irnanc <irnanc@gmail.com>
2014-02-15 00:16:28 -0800

    First urls

--- WebApp/intime/intime/urls.py
+++ WebApp/intime/intime/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from trackemployee import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +10,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.login, name='login'),
+    #The main page should check to see if the user is authenticated, if s/he is. Direct to login, otherwise direct to main.
+    url(r'^index$', views.index, name='index'),
+    url(r'^employees$', views.employees, name='employees'),
+    url(r'^workplace$', views.employees, name='workplace'),
 )
